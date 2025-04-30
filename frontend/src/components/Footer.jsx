ae282f116b2ab8197fad4507bec53ae5f65fe0a2
Himanshu Kumar <himanshukr0008@gmail.com>
2025-05-01 01:27:30 +0530

    a

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,10 +8,10 @@ const Footer = () => {
         <p>&copy; 2025 Himanshu Kumar. All Rights Reserved.</p>
       </div>
       <div>
-        <a href="https://www.linkedin.com/in/himanshu" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/himanshu-kumar-23993422a" target="_blank" rel="noopener noreferrer">
           LinkedIn
         </a> |
-        <a href="mailto:himanshu@example.com" target="_blank" rel="noopener noreferrer">
+        <a href="mailto:himanshukr0008@gmail.com" target="_blank" rel="noopener noreferrer">
           Email
         </a>
       </div>
